0efa40f87dabc299609a7df8a945fb92a93d1b20
Adrien Delorme <azr@users.noreply.github.com>
2025-06-17 11:39:37 +0200

    fix when multipart fetching and the server does not return content length
    Signed-off-by: Adrien Delorme <azr@users.noreply.github.com>

--- core/remotes/docker/fetcher.go
+++ core/remotes/docker/fetcher.go
@@ -478,6 +478,10 @@ func (r dockerFetcher) open(ctx context.Context, req *request, mediatype string,
 	})
 
 	remaining, _ := strconv.ParseInt(resp.Header.Get("Content-Length"), 10, 0)
+	if remaining <= minChunkSize {
+		parallelism = 1
+	}
+
 	if parallelism > 1 && req.body == nil {
 		// If we have a content length, we can use multiple requests to fetch
 		// the content in parallel. This will make download of bigger bodies
--- core/remotes/docker/fetcher_test.go
+++ core/remotes/docker/fetcher_test.go
@@ -129,6 +129,7 @@ func TestFetcherOpenParallel(t *testing.T) {
 	size := int64(3 * 1024 * 1024)
 	content := make([]byte, size)
 	rand.New(rand.NewSource(1)).Read(content)
+	sendContentLength := true
 
 	f := dockerFetcher{
 		&dockerBase{
@@ -175,12 +176,16 @@ func TestFetcherOpenParallel(t *testing.T) {
 		}
 
 		if len(rng) == 0 {
-			rw.Header().Set("content-length", strconv.Itoa(len(content)))
+			if sendContentLength {
+				rw.Header().Set("content-length", strconv.Itoa(len(content)))
+			}
 			_, _ = rw.Write(content)
 		} else {
 			b := content[rng[0].start : rng[0].start+rng[0].length]
 			rw.Header().Set("content-range", rng[0].contentRange(size))
-			rw.Header().Set("content-length", strconv.Itoa(len(b)))
+			if sendContentLength {
+				rw.Header().Set("content-length", strconv.Itoa(len(b)))
+			}
 			_, _ = rw.Write(b)
 		}
 
@@ -232,6 +237,10 @@ func TestFetcherOpenParallel(t *testing.T) {
 	checkReader(0)
 
 	checkReader(25)
+
+	sendContentLength = false
+	checkReader(25)
+	sendContentLength = true
 
 	ignoreContentRange = true
 	checkReader(25)
